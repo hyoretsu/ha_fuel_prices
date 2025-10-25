06e1062623f6d2c234c834e7defa8e52328a29d7
Aran Leite <hyoretsu@gmail.com>
2025-10-25 12:11:05 -0300

    Bump

--- custom_components/ha_fuel_prices/config_flow.py
+++ custom_components/ha_fuel_prices/config_flow.py
@@ -1,7 +1,8 @@
-from homeassistant import config_entries
 import voluptuous as vol
+from homeassistant import config_entries
 
 from .const import DOMAIN
+
 
 class FuelPricesConfigFlow(config_entries.ConfigFlow, domain=DOMAIN):
     """Configuração do fluxo de integração para preços de combustíveis."""
@@ -14,9 +15,11 @@ class FuelPricesConfigFlow(config_entries.ConfigFlow, domain=DOMAIN):
             return self.async_create_entry(title="Fuel Prices", data=user_input)
 
         # Esquema de configuração do usuário
-        schema = vol.Schema({
-            vol.Required("state", default="SANTA CATARINA"): str,
-            vol.Required("city", default="TUBARAO"): str,
-        })
+        schema = vol.Schema(
+            {
+                vol.Required("state", default="PARAIBA"): str,
+                vol.Required("city", default="JOAO PESSOA"): str,
+            }
+        )
 
         return self.async_show_form(step_id="user", data_schema=schema)
